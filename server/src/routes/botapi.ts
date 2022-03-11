d41ebff7ed2747e0873f0ef6ec9b14f6a59f4218
teoet6 <teoet6@protonmail.com>
2022-03-11 11:19:52 +0200

    Add a webinterface and simulated latency

--- server/src/routes/botapi.ts
+++ server/src/routes/botapi.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import fileUpload from 'express-fileupload';
 
+import config from '../config'
+
 const router = Router();
 
-router.use(fileUpload({
+router.use(fileUpload({}));
 
-}));
+router.use((req, res, next) => {
+    setTimeout(next, config.simulatedLatency);
+});
 
 router.get('/status', (req, res) => {
     res.send('Bot is kinda ok');
