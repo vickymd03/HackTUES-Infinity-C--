6bfb28fe0787ec19afe9ce825e35e8b853814cae
Victoria Dimitrova <vickymdimitrova@gmail.com>
2022-03-11 13:24:45 +0200

    Split HTML, CSS, JS into different files

---
+++ server/views/helpers.js
@@ -0,0 +1,17 @@
+var Video = {
+    canvas : document.createElement("canvas"),
+    stream : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 350;
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
